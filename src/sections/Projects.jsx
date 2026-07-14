50fdf136c17dfb4cdfb0e4787d37c1039cb1ee82
Balaji <udata677@gmail.com>
2026-07-14 10:20:22 +0530

    updating project link

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,6 +1,5 @@
 import { ArrowUpRight } from "lucide-react";
 import { FaGithub } from "react-icons/fa6";
-import { AnimatedBorderButton } from "@/components/AnimatedBorderButton";
 const projects = [
   {
     title: "LMS",
@@ -144,10 +143,10 @@ export const Projects = () => {
 
         {/* View All CTA */}
         <div className="text-center mt-12 animate-fade-in animation-delay-500">
-          <AnimatedBorderButton>
+          <button className=" bg-violet-500 p-3 m-auto rounded-2xl hover:border-primary/50 animated-border ">
             View All Projects
             <ArrowUpRight className="w-5 h-5" />
-          </AnimatedBorderButton>
+          </button>
         </div>
       </div>
     </section>
